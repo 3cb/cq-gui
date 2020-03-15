b5164336549a8a0942b818516678f459fe18968a
marc <marc.cbrooks@gmail.com>
2020-03-14 23:23:50 -0400

    change color white from constant to rgba value

--- cq/color.go
+++ cq/color.go
@@ -3,9 +3,9 @@ package cq
 import "image/color"
 
 var (
-	green = &color.RGBA{R: 0, G: 230, B: 64, A: 1}
-	red   = &color.RGBA{R: 207, G: 0, B: 15, A: 1}
-	white = color.White
+	green = color.RGBA{R: 0, G: 230, B: 64, A: 1}
+	red   = color.RGBA{R: 207, G: 0, B: 15, A: 1}
+	white = color.RGBA{R: 255, G: 255, B: 255, A: 1}
 )
 
 func setColor(c PriceChange) color.Color {
